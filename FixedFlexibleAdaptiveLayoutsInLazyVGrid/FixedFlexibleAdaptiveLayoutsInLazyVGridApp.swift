d4e08c2aca4b7f5c65bccfcccf9d3aeeecc61aea
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-06-30 21:10:33 +0300

    Initial Commit

---
+++ FixedFlexibleAdaptiveLayoutsInLazyVGrid/FixedFlexibleAdaptiveLayoutsInLazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixedFlexibleAdaptiveLayoutsInLazyVGridApp.swift
+//  FixedFlexibleAdaptiveLayoutsInLazyVGrid
+//
+//  Created by Abdelrahman Mohamed on 30.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct FixedFlexibleAdaptiveLayoutsInLazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
