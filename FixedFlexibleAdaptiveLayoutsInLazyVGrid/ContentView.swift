94af51ec4efb77405ac8bcad7b854ccba4d25e6c
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-06-30 21:57:19 +0300

    feat(users): Understanding Fixed, Flexible and Adaptive Layouts in LazyVGrid (#1)
    Fixed, Flexible and Adaptive

--- FixedFlexibleAdaptiveLayoutsInLazyVGrid/ContentView.swift
+++ FixedFlexibleAdaptiveLayoutsInLazyVGrid/ContentView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let columns: [GridItem] = [
+        GridItem(.fixed(100)),
+        GridItem(.fixed(100)),
+        GridItem(.fixed(100)),
+        GridItem(.fixed(100))
+    ]
+
     var body: some View {
-        Text("Hello, world!").padding()
+
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(1..<100) { _ in
+                        Rectangle()
+                            .fill(Color.red)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+            }
+            .navigationTitle("Fixed, Flexible, Adaptive Layouts in LazyVGrid")
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11 Pro Max")
+        }
     }
 }
